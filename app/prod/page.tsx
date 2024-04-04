174b545888990b7a390f6b20e523836064937e69
biggeye <justin_bonner@protonmail.com>
2024-04-04 14:13:57 -0700

    .
    undefined

--- app/prod/page.tsx
+++ app/prod/page.tsx
@@ -1,9 +1,10 @@
-import { Text } from "@chakra-ui/react"
+import { VStack, Text } from "@chakra-ui/react"
 
 const ProductionPage = () => {
 
     return(
-        <Text>Welcome.</Text>
+        <VStack><Text mt="15px">Welcome.</Text>
+    </VStack>
     )
 }
 
---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+import ImageCreator from "@/components/prod/ImageCreator";
+
+const ImageCreatorTestPage = () => {
+    return(
+        <ImageCreator />
+    )
+}
+
+export default ImageCreatorTestPage;
